26c344c5d3ea1802f7c844d0fa3e7aa528c321e9
Adnan Mahmud <11adnan@live.com>
2023-05-12 01:29:19 +0600

    clearitem and remove reducer added

--- starter/src/components/CartContainer.js
+++ starter/src/components/CartContainer.js
@@ -1,29 +1,43 @@
-import CartItem from "./CartItem"
-import { useSelector } from "react-redux"
+import CartItem from "./CartItem";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
 
 const CartContainer = () => {
-const { cartItems, total, amount } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
 
-console.log(cartItems);
-if(amount <1){
-    return <section className="cart">
+  console.log(cartItems);
+  if (amount < 1) {
+    return (
+      <section className="cart">
         <header>
-            <h2>your bag</h2>
-            <h4 className="empty-cart">is currently empty</h4>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
         </header>
-    </section>
-}
-
+      </section>
+    );
+  }
 
   return (
     <section className="cart">
-        <header>
-            <h2>your bag </h2>
-        </header>
-        <div>{cartItems.map((item)=>{
-            return <CartItem key={item.id}/>
-        })}</div>
+      <header>
+        <h2>your bag </h2>
+      </header>
+      <div>
+        {cartItems.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </div>
+      <footer>
+        <div className="cart-total">
+          <hr />
+          <h4>
+            total <span>${total}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn" onClick={()=> dispatch(clearCart())}>clear cart</button>
+      </footer>
     </section>
-  )
-}
+  );
+};
 export default CartContainer;
--- starter/src/components/CartItem.js
+++ starter/src/components/CartItem.js
@@ -1,6 +1,34 @@
-const CartItem = () => {
+import React from "react";
+import { ChevronDown, ChevronUp } from "../icons";
+import { removeItem } from "../features/cart/cartSlice";
+import { useDispatch } from "react-redux";
+
+
+const CartItem = ({ id, img, title, price, amount }) => {
+
+  const dispatch= useDispatch();
   return (
-    <div>CartItem</div>
-  )
-}
-export default CartItem
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
+        <button className="remove-btn" onClick={()=>{dispatch(removeItem(id))}}>remove</button>
+      </div>
+      <div>
+        {/* increase amount */}
+        <button className="amount-btn">
+          <ChevronUp />
+        </button>
+        {/* amount */}
+        <p className="amount">{amount}</p>
+        {/* decrease amount */}
+        <button className="amount-btn">
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default CartItem;
