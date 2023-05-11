6fc35e6e5c950b997ff308468779476ff1e66310
DESKTOP-NOMKETR\adnan <11adnan@live.com>
2023-05-11 13:38:21 +0600

    cart configured

---
+++ starter/src/components/CartContainer.js
@@ -0,0 +1,29 @@
+import CartItem from "./CartItem"
+import { useSelector } from "react-redux"
+
+const CartContainer = () => {
+const { cartItems, total, amount } = useSelector((store) => store.cart);
+
+console.log(cartItems);
+if(amount <1){
+    return <section className="cart">
+        <header>
+            <h2>your bag</h2>
+            <h4 className="empty-cart">is currently empty</h4>
+        </header>
+    </section>
+}
+
+
+  return (
+    <section className="cart">
+        <header>
+            <h2>your bag </h2>
+        </header>
+        <div>{cartItems.map((item)=>{
+            return <CartItem key={item.id}/>
+        })}</div>
+    </section>
+  )
+}
+export default CartContainer;
